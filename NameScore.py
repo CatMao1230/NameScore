5fbc24cf660cb73f6ed385a3b50be0ee57ba58e7
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 11:24:37 +0800

    Use function and list comprehension to simplify code

--- NameScore.py
+++ NameScore.py
@@ -1,14 +1,15 @@
 import sys
 
-score = []
-with open(sys.argv[1]) as fo:
-	line = fo.readline()
+def access_from_file(file):
+	with open(file) as fo:
+		line = fo.readline()
+	line = line.strip().replace('"', '')
+	score = line.split(',')
+	score.sort()
+	return score
 
-line = line.strip().replace('"', '')
-score = line.split(',')
-score.sort()
-for index, element in enumerate(score):
-	sum = 0
-	for j in element:
-		sum += ord(j) - 64
-	print sum * (index + 1)
+def name_score(score):
+	for index, element in enumerate(score):
+		print sum([ord(x)-64 for x in element]) * (index + 1)
+
+name_score(access_from_file(sys.argv[1]))
