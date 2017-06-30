c67571164366cb7289e85f5c496e8ea300ad53b5
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-30 09:32:23 +0800

    Add two units to test fix_str is correct, and one unit to test empty string's score

--- TestNameScore.py
+++ TestNameScore.py
@@ -7,12 +7,26 @@ class TestNameScore(unittest.TestCase):
 		expacted_value = ['ABC', 'CDEF', 'FIJK']
 		value = NameScore.access_from_file(file_name)
 		self.assertEqual(expacted_value, value)
-	def test_name_score(self):
+	def test_fix_str_1(self):
+		ls = '"aB C","CD***EF","FI JK"'
+		expacted_value = ['ABC', 'CDEF', 'FIJK']
+		value = NameScore.fix_str(ls)
+		self.assertEqual(expacted_value, value)
+	def test_fix_str_2(self):
+		ls = '"98YA--","     ","\\\\!!!O"'
+		expacted_value = ['YA', '', 'O']
+		value = NameScore.fix_str(ls)
+		self.assertEqual(expacted_value, value)
+	def test_name_score_1(self):
 		ls = ['ABC', 'CDEF', 'FIJK']
 		expacted_value = [6, 36, 108]
 		value = NameScore.name_score(ls)
 		self.assertEqual(expacted_value, value)
-
+	def test_name_score_2(self):
+		ls = ['A', 'CD', '']
+		expacted_value = [1, 14, 0]
+		value = NameScore.name_score(ls)
+		self.assertEqual(expacted_value, value)
 
 if __name__ == '__main__':
 	unittest.main()
